9a93cb922ad9d60297cffbe9aa2c26549500670f
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2017-10-17 00:09:59 +0530

    Question 8 completed

--- Q8/t_Reg32b.v
+++ Q8/t_Reg32b.v
@@ -1,5 +1,5 @@
 /**
-*	Test bench for D flip-flop
+*	Test bench for 32 bit Register using D flip-flop
 *	By: Omkar Prabhu 16CO233
 *	16th October 2017
 */
@@ -13,9 +13,11 @@ module t_Reg32b;
 	// inputs 
 	reg [0:31] data_in;
 	reg clk;
+	// active low reset signal
+	reg res;
 	
 	// 32 bit Register thread instantiated
-	Reg32b inst (data_out, data_in, clk);
+	Reg32b inst (data_out, data_in, clk, res);
 	
 	integer i;
 	// check for various input combinations
@@ -26,12 +28,18 @@ module t_Reg32b;
 		
 		data_in = 32'b00000000000000000000000000000000;
 		clk = 1'b0;
+		res = 1'b1;
 			
 		for (i = 1; i <= 20; i = i + 1) begin
     		#6 data_in = data_in + 1'b1;
    		end
 			
 	end	
+
+	initial begin
+		#10 res = 1'b1;
+		#24 res = 1'b0;
+	end
 	
 	initial begin
 		repeat (30) begin
@@ -41,7 +49,7 @@ module t_Reg32b;
 	
 	// print result
 	initial begin
-		//$monitor("%t: d: %b clk: %b \t\t\t q: %b q': %b\n", $time, d, clk, q, qn);	
+		//$monitor("%t: d: %b clk: %b reset: %b \t\t\t q: %b q': %b\n", $time, d, clk, res, q, qn);	
 	end
 	
 	
